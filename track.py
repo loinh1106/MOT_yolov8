99f9fcc24a042782fc362af4d99c299db9a7662e
hoang loi <loinh20406c@st.uel.edu.vn>
2023-02-28 14:29:46 +0700

    fix frame idx

--- track.py
+++ track.py
@@ -249,7 +249,7 @@ def run(
                             bbox_h = output[3] - output[1]
                             # Write MOT compliant results to file
                             with open(txt_path + '.txt', 'a') as f:
-                                f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,  # MOT format
+                                f.write(('%g ' * 10 + '\n') % (frame_idx, id, bbox_left,  # MOT format
                                                                bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))
 
                         if save_vid or save_crop or show_vid:  # Add bbox/seg to image
